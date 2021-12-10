036713ad2fec6b80d58b87930f647f95a7e76530
namookk <papakang22@naver.com>
2021-12-10 15:11:00 +0900

    프로젝트 분리작업

---
+++ src/main/java/com/mycloset/common/MyClosetCommonApplication.java
@@ -0,0 +1,13 @@
+package com.mycloset.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClosetCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyClosetCommonApplication.class, args);
+    }
+
+}
