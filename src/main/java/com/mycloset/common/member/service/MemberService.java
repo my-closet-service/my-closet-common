c1cdc4b1562de0c32506f3d3e03b7072a4886192
JEONGHYUNJUNE <gtyb08@naver.com>
2021-12-13 19:42:15 +0900

    COMMON - MEMBER 삭제

--- src/main/java/com/mycloset/common/member/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.mycloset.common.member.service;
-
-import com.mycloset.common.member.entity.Member;
-
-import java.util.List;
-
-public interface MemberService {
-    List<Member> findAll();
-}
